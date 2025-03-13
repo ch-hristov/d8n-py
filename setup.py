0ef8a62f179159fe8f3eb6d9020cf70f1a645ad5
Christo S. Christov <ch-hristov@users.noreply.github.com>
2025-03-12 22:09:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='d8n',
-    version='0.3.3',
+    version='0.4.0',
     author='Christo',
     author_email='christochristov@duck.com',
     description='d8n API python library',
