dcd055cb9551065e1c59bd031a6c499204981e9e
Christo S. Christov <ch-hristov@users.noreply.github.com>
2025-03-15 11:03:02 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,6 @@ client = d8nClient("MY-API-KEY")
 
 request = client.from_local_file("./large.jpg")
 result_document = client.wait_till_completed(request['id'], print_debug_info=True)
-results = client.get_completed(request['id'])
 
 for task in result_document.task_list:
     print(task)
